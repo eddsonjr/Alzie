e21ca183c638535bdbe156cd3c8fb7cde560af5d
Edson Jr <edsonjr@macbook-pro-de-edson.local>
2016-07-04 17:00:10 -0400

    Projeto Criado

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform:ios, 9.0
+use_frameworks!
+
+target 'Alziegit’ do
+	pod 'RealmSwift'
+
+end
+
+
---
+++ Pods/Target Support Files/Pods-Alziegit/Pods-Alziegit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Alziegit : NSObject
+@end
+@implementation PodsDummy_Pods_Alziegit
+@end

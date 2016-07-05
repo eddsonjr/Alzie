4e9499f428508a1a5dd699c0611dc7bbba5114e2
Carlos Machado <carlos.hv.machado@icloud.com>
2016-07-05 15:39:42 -0400

    Tentando

--- Alziegit/ViewController.swift
+++ Alziegit/ViewController.swift
@@ -16,8 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        print("asdasd")
-        
+    
     }
 
     override func didReceiveMemoryWarning() {
